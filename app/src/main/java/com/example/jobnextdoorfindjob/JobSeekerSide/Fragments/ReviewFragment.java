30c9b09a1f1eefa654371e09f2e7c4c63a39ab92
shahzaibkhan1 <zaibidev471@gmail.com>
2022-09-26 16:32:30 +0500

    first time

---
+++ app/src/main/java/com/example/jobnextdoorfindjob/JobSeekerSide/Fragments/ReviewFragment.java
@@ -0,0 +1,16 @@
+package com.example.jobnextdoorfindjob.JobSeekerSide.Fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.jobnextdoorfindjob.R;
+
+public class ReviewFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_fragment);
+    }
+}
